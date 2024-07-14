5940d5e8f96ac71e34708749f05082507c9bc9f4
DlopezS98 <01dlopezs98@gmail.com>
2024-07-13 23:45:47 -0600

    implement drawer and app bar for user navigation

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import App from "./App";
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./modules/auth/components/login";
+import Discover from "./modules/discover/components/discover";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+		children: [
+			{ path: "/discover", Component: Discover },
+		]
 	},
 	{
 		path: "/login",
